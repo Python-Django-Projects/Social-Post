75ed867b63196a14e03624e31f0d4b1b5d17dc11
mpandhp@gmail.com <mpandhp@gmail.com>
2023-03-20 13:11:14 -0400

    signup

--- frontend/web/src/screens/auth/signin/SignIn.js
+++ frontend/web/src/screens/auth/signin/SignIn.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import '../../../scss/auth.scss'
 import { signin_api } from "../../../services";
-
+import { Button, Form, Input } from "antd";
+import Checkbox from "antd/es/checkbox/Checkbox";
+// import "antd/dist/antd.css";
 
 
 function SignIn() {
@@ -35,9 +37,43 @@ function SignIn() {
 
   return (
     <div>
-        <div className="form-container">
+       {/* <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+   >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form> */}
             <form onSubmit={handleSubmit}>
-            {/* {error && <div>{error}</div>} */}
             <label htmlFor="email">Email:</label>
             <input
                 type="email"
@@ -56,10 +92,11 @@ function SignIn() {
             <span> Invalid Password </span>:''}
             <button type="submit">Sign In</button>
             <button>Forgot password?</button>
+            <Button type="primary">check me!</Button>
             <button><a href="/signup">register</a></button>
             </form>
+
         </div>
-    </div>
   );
 }
 
--- frontend/web/src/services/index.js
+++ frontend/web/src/services/index.js
@@ -18,3 +18,23 @@ export async function signin_api(email,password){
         
       })
 }
+
+
+export async function signup_api(username,email,password) {
+  return fetch("http://127.0.0.1:8000/auth/signup/", {
+    method: "POST",
+
+    headers: { "Content-Type": "application/json" },
+
+    body: JSON.stringify({ username, email, password }),
+
+  }).then(res => res.json()).then(response => {
+    console.log("response: ",response)
+    return response;
+
+  }).catch(error => {
+    console.log("error: ",error.message)
+    return error.message;
+    
+  })
+}
